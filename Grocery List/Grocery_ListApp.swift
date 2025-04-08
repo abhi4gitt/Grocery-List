8ce79501cd61f6e6b53077379978e0fdd3b4f563
abhi4gitt <raiabhishek631@gmail.com>
2025-04-08 22:04:05 +0530

    app setup with app icon

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Abhishek on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
